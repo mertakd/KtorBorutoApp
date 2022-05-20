7dc842faf734b21959db1cc491b899ed444af54f
Mert Akdoğan <mertfalcone01@gmail.com>
2022-05-20 17:33:11 +0300

    Procfile added and gradle build file modified

---
+++ src/main/kotlin/com/example/di/KoinModule.kt
@@ -0,0 +1,16 @@
+package com.example.di
+
+import com.example.repository.HeroRepository
+import com.example.repository.HeroRepositoryImpl
+import org.koin.dsl.module
+
+val koinModule = module {
+    single<HeroRepository> {
+        HeroRepositoryImpl()
+    }
+
+
+    /*
+    -singleton oluşturuyoruz
+     */
+}
